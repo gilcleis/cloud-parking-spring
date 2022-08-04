54f4687d69d25c637777bfc58f74a65e417f97b1
gil <gilclei@gmail.com>
2022-08-03 23:52:24 -0300

    Aplicando Design Patterns DTO para representar nossa view. Finalizando a criação do endpoint de cadastro.

--- src/main/java/com/gilclei/cloudparking/controller/ParkingController.java
+++ src/main/java/com/gilclei/cloudparking/controller/ParkingController.java
@@ -2,27 +2,39 @@ package com.gilclei.cloudparking.controller;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gilclei.cloudparking.controller.dto.ParkingDTO;
+import com.gilclei.cloudparking.controller.mapper.ParkingMapper;
 import com.gilclei.cloudparking.model.Parking;
+import com.gilclei.cloudparking.service.ParkingService;
 
 @RestController
 @RequestMapping("/parking")
 public class ParkingController {
 	
+	
+	private final ParkingService parkingService;
+	private final ParkingMapper parkingMapper;
+	
+	public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+		this.parkingService = parkingService;
+		this.parkingMapper = parkingMapper;
+	}
+	
+	
+	
 	@GetMapping
-	public List<Parking> findAll(){
-		var parking = new Parking();
-		parking.setColor("PRETO");
-		parking.setLicense("MSS-1111");
-		parking.setModel("vw GOL");
-		parking.setState("SP");
-		
-		return Arrays.asList(parking);
+	public List<ParkingDTO> findAll(){
+		List<Parking> parkingList = parkingService.findAll();
+		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+		return result;
 	}
 
 }
